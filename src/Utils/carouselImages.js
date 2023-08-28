29755f3fc001a3a239f5ef78940d125afa2c82bc
Igor Zupunski <ipatheword@live.com>
2023-08-28 16:33:33 +0200

    made the api working

--- src/Utils/carouselImages.js
+++ src/Utils/carouselImages.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+const accessKey = "h6EuMvEw3fpcIipwntO1GEWNbYt7n-tzEAbvSTSZ_88";
+
 const fetchImages = async () => {
     try {
-        const response = await axios.get("API_ENDPOINT");
-        return response.data;
+        const response = await axios.get(`https://api.unsplash.com/photos/random?count=5&client_id=${accessKey}`);
+        const imageUrls = response.data.map((image) => image.urls.regular);
+        return imageUrls;
     } catch (error) {
         console.error("Error fetching images: ", error);
         return [];
