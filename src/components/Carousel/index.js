4e9f3612bd34004ed4475820466639dc5fa0173e
Igor Zupunski <ipatheword@live.com>
2023-08-29 10:44:34 +0200

    small adjustments

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -25,27 +25,31 @@ export default function Carousel() {
 
     return(
         <div className="carousel-container">
-            <Button
-                onClick={goToPreviousSlide}
-                variant="carousel-button backwards"
-            >
-                Backwards
-            </Button>
-            {images.length > 0 && (
-                <div className="carousel__image-container">
-                    <img 
-                        className="carousel-image"
-                        src={images[currentIndex]}
-                        alt={`Slide ${currentIndex}`}
-                    />
+            <div className="carousel__content">
+                {images.length > 0 && (
+                    <div className="carousel__image-container">
+                        <img 
+                            className="carousel-image"
+                            src={images[currentIndex]}
+                            alt={`Slide ${currentIndex}`}
+                        />
+                    </div>
+                )}
+                <div className="carousel__button-box">
+                    <Button
+                        onClick={goToPreviousSlide}
+                        variant="carousel-button"
+                    >
+                        Backwards
+                    </Button>
+                    <Button
+                        onClick={goToNextSlide}
+                        variant="carousel-button"
+                    >
+                        Forwards
+                    </Button>
                 </div>
-            )}
-            <Button
-                onClick={goToNextSlide}
-                variant="carousel-button forwards"
-            >
-                Forwards
-            </Button>
+            </div> 
         </div>
     );
 }
